e9d5640425cccdadcc9f1c512ac0577a96e4e90e
hawiju <31375101+hawiju@users.noreply.github.com>
2018-04-08 18:50:30 +0300

    ni

--- script1.py
+++ script1.py
@@ -1,11 +1,19 @@
 print('''let's open a file''')
+import sys
+columns=4
+fil='file.csv'
+if len(sys.argv) ==2 :
+    columns=int(sys.argv[1])
+elif len(sys.argv)>2:
+    fil=sys.argv[1]
+    columns=int(sys.argv[2])
+
 with open('file.csv') as table:
     s=table.readlines()
     ns=[]
     for l in s:
         ns.append(l.replace('\n',','))
-
-for i in range(3,len(ns),4):
+for i in range(columns-1,len(ns),columns):
     ns[i]=ns[i].replace(',','\n')
 
 with open('out.csv',mode='w')  as targ:
