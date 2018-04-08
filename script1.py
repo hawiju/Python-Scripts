8177304331d4548c11087161a3821183815e4b4c
hawiju <31375101+hawiju@users.noreply.github.com>
2018-04-08 18:00:53 +0300

    yes

--- script1.py
+++ script1.py
@@ -1,4 +1,13 @@
 print('''let's open a file''')
 with open('file.csv') as table:
     s=table.readlines()
-    print(s)
+    ns=[]
+    for l in s:
+        ns.append(l.replace('\n',','))
+
+for i in range(3,len(ns),4):
+    ns[i]=ns[i].replace(',','\n')
+
+with open('out.csv',mode='w')  as targ:
+    for item in ns:
+        targ.write(item)
